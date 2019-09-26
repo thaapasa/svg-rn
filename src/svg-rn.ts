de358c42aeda8e4db5c01d4de56d07a975c45870
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2019-09-26 21:40:23 +0300

    Do input/output based on CLI opts

--- src/svg-rn.ts
+++ src/svg-rn.ts
@@ -3,17 +3,35 @@ import { svgToReactNative } from './svg-to-rn';
 import * as fs from 'fs';
 import { log } from './util/logger';
 import { cliOptions } from './cli';
+import { promisify } from 'util';
 
-function output(data: string) {
-  console.log(data);
+const readFileP = promisify(fs.readFile);
+const writeFileP = promisify(fs.writeFile);
+
+async function getInput(): Promise<string> {
+  if (cliOptions.input) {
+    // Read input file
+    const buf = await readFileP(cliOptions.input, { encoding: 'utf-8' });
+    return buf.toString();
+  }
+  // Read stdin
+  // See https://stackoverflow.com/questions/30441025/read-all-text-from-stdin
+  const buf = await readFileP(0);
+  return buf.toString();
+}
+
+async function doOutput(data: string) {
+  if (cliOptions.output) {
+    await writeFileP(cliOptions.output, data, { encoding: 'utf-8' });
+  } else {
+    process.stdout.write(data);
+  }
 }
 
 async function run() {
-  const opts = cliOptions;
-  log('svg-rn starts with opts', opts);
-
-  const data = fs.readFileSync('test/wikimedia-test.svg').toString();
-  output(await svgToReactNative(data));
+  const inputSvg = await getInput();
+  const outputJsx = await svgToReactNative(inputSvg);
+  await doOutput(outputJsx);
 }
 
 run().catch(e => log(`Error when running svg-rn: ${e}`));
--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,3 +1,8 @@
+function toLogStr(x: any): string {
+  return typeof x === 'string' ? x : JSON.stringify(x, null, 2);
+}
+
 export function log(...msg: any[]) {
-  console.log(...msg);
+  const message = msg.map(toLogStr).join(' ');
+  process.stderr.write(message + '\n');
 }
