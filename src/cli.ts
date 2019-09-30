a6987f06a802051f1bf086b87fa4ef18ea341379
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2019-09-30 21:04:36 +0300

    Refactor project structure, configure package.json

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,11 @@
 import program from 'commander';
+import { svgToReactNative } from './svg-to-rn';
+import * as fs from 'fs';
+import { log } from './util/logger';
+import { promisify } from 'util';
+
+const readFileP = promisify(fs.readFile);
+const writeFileP = promisify(fs.writeFile);
 
 interface CommandLineOptions {
   output?: string;
@@ -12,4 +19,32 @@ program
 
 program.parse(process.argv);
 
-export const cliOptions = program.opts() as CommandLineOptions;
+const cliOptions = program.opts() as CommandLineOptions;
+
+async function getInput(): Promise<string> {
+  if (cliOptions.input) {
+    // Read input file
+    const buf = await readFileP(cliOptions.input, { encoding: 'utf-8' });
+    return buf.toString();
+  }
+  // Read stdin
+  // See https://stackoverflow.com/questions/30441025/read-all-text-from-stdin
+  const buf = await readFileP(0, { encoding: 'utf-8' });
+  return buf.toString();
+}
+
+async function doOutput(data: string) {
+  if (cliOptions.output) {
+    await writeFileP(cliOptions.output, data, { encoding: 'utf-8' });
+  } else {
+    process.stdout.write(data);
+  }
+}
+
+async function run() {
+  const inputSvg = await getInput();
+  const outputJsx = await svgToReactNative(inputSvg);
+  await doOutput(outputJsx);
+}
+
+run().catch(e => log(`Error when running svg-rn: ${e}`));
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { svgToReactNative } from './svg-to-rn';
+
+export default svgToReactNative;
